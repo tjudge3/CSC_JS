878a6e459142ae424347b27c2a117af24ba6fac9
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2024-06-09 05:00:20 -0400

    Delete theClickWeb directory

--- theClickWeb/js/main.js
+++
@@ -1,9 +0,0 @@
-function hide(){
-    let imgWeb=document.querySelector("#imgWeb");
-    let nextComputer=document.querySelector(".nextComputer");
-    let h1=document.querySelector("h1");
-
-    imgWeb.style.display="none";
-    nextComputer.style.fontSize = "2em";
-    h1.style.color="grey";
-}
