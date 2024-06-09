780dae0a80d43b8a6c12202654cd470e35800e31
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2024-06-09 05:10:05 -0400

    Add files via upload

---
+++ assignments/rankPhotos/js/project05-02.js
@@ -0,0 +1,33 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-02
+
+      Project to move images between a photo bucket and photo list.
+      Author: myName(Tom)
+      Date: theDate
+      Updated: theDateLast  
+
+      Filename: project05-02.js
+*/
+
+let images = document.querySelectorAll("img");
+let photoBucket = document.querySelector("#photo_bucket");
+let photoList = document.querySelector("#photo_list");
+
+for (let i = 0; i < images.length; i++){
+      images[i].onclick = function(){
+            if (this.closest("#photo_bucket")){
+                  let newItem = document.createElement("li");
+                  photoList.appendChild(newItem);
+                  newItem.appendChild(this);
+            }
+            else {
+                  let oldItem = this.closest("li");
+                  photoBucket.appendChild(this);
+                  if (oldItem){
+                        oldItem.parentElement.removeChild(oldItem);
+                  }
+            }
+      };
+}
---
+++ assignments/theClickWeb/js/main.js
@@ -0,0 +1,9 @@
+function hide(){
+    let imgWeb=document.querySelector("#imgWeb");
+    let nextComputer=document.querySelector(".nextComputer");
+    let h1=document.querySelector("h1");
+
+    imgWeb.style.display="none";
+    nextComputer.style.fontSize = "2em";
+    h1.style.color="grey";
+}
